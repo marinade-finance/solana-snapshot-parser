ecc04beb631bfe5a9a8f423d44764d8ca532d1f2
janlegner <jan.legner@gmail.com>
2025-04-03 11:10:12 +0200

    fix subshell exiting

--- scripts/fetch-snapshot.bash
+++ scripts/fetch-snapshot.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 
 epoch="$1"
 target_dir="$2"
@@ -23,7 +24,7 @@ echo "Target path: $target_dir_absolute" >&2
 marinade_gs_bucket="gs://marinade-solana-snapshot-mainnet"
 jito_gs_bucket="gs://jito-mainnet"
 
-gs_files=$(gcloud storage ls "$marinade_gs_bucket/$epoch/**/*.tar.zst" || gcloud storage ls "$jito_gs_bucket/$epoch/**/*.tar.zst" || exit 1)
+gs_files=$(gcloud storage ls "$marinade_gs_bucket/$epoch/**/*.tar.zst" || gcloud storage ls "$jito_gs_bucket/$epoch/**/*.tar.zst")
 echo "Available objects:" >&2
 echo "$gs_files" >&2
 
--- scripts/list-last-jito-snapshots.bash
+++ scripts/list-last-jito-snapshots.bash
@@ -1,3 +1,6 @@
 #!/bin/bash#!/bin/bash
 
-gstorage_items=$(gcloud storage ls --recursive gs://jito-mainnet || exit 1)
+set -e
+set -o pipefail
+
+gstorage_items=$(gcloud storage ls --recursive gs://jito-mainnet)
